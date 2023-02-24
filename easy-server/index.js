3c0f7d868da63af79ec1faed3e8a064146694993
blake <blake@localhost.localdomain>
2023-02-24 14:46:25 -0600

    remove debug messages and commented code

--- easy-server/index.js
+++ easy-server/index.js
@@ -31,52 +31,12 @@ function scrapeForNewPosts() {
 		});
 }
 
-// JSDOM
-// 	.fromURL("https://memo.cash/posts/new")
-// 	.then(function (dom) {
-// 		console.log('got dom', dom.window.document.documentElement.outerHTML);
-
-// 		var body = dom.window.document.body;
-
-// 		//var posts = body.getElementsByTagName('post');
-// 		var postsNodes = dom.window.document.querySelectorAll('.post');
-
-// 		/*
-// 			.post_header
-// 				.name
-// 					.mini-profile-name
-// 						img.identicon [src]
-// 						a.profile [href & innerText]
-// 					a.time-ago [title]
-// 			.message [innerText]
-		
-// 		*/
-
-// 		console.log('found', postsNodes.length, 'posts?');
-
-// 		var posts = [];
-// 		var haveNew = false;
-// 		for (let i = 0; i < postsNodes.length; i++) {
-// 			var post = scrapePost(postsNodes[i]);
-// 			posts.push(post);
-// 			if (postMap[post.txt] === undefined) haveNew = true;
-// 			postMap[post.tx] = post;
-// 		}
-
-// 		console.log(JSON.stringify(posts));
-// 		console.log(posts);
-// 	})
-// 	.catch(function (err) {
-// 		console.log(err);
-// 	});
-
 function scrapePost(postNode) {
-	console.log('postNode', postNode.outerHTML);
 	var post = {tx: postNode.attributes['data-tx-hash'].textContent};
 
 	var header = postNode.querySelector('.post-header');
 	var nameNode = header.querySelector('.name');
-	//console.log('nameNode', nameNode.outerHTML);
+	
 	var identicon = nameNode.querySelector('.mini-profile-name .profile-pic-24');
 	var name = nameNode.querySelector('.mini-profile-name a.profile');
 	post.name = {link: name.href, text: name.textContent};
@@ -92,11 +52,8 @@ function getNewPosts() {
 	return JSDOM
 			.fromURL("https://memo.cash/posts/new")
 			.then(function (dom) {
-				console.log('got dom', dom.window.document.documentElement.outerHTML);
-
 				var body = dom.window.document.body;
 
-				//var posts = body.getElementsByTagName('post');
 				var postsNodes = dom.window.document.querySelectorAll('.post');
 
 				/*
@@ -110,22 +67,7 @@ function getNewPosts() {
 				
 				*/
 
-				console.log('found', postsNodes.length, 'posts?');
-
 				return Array.prototype.map.call(postsNodes, scrapePost);
-
-				// var posts = [];
-				// var haveNew = false;
-				// for (let i = 0; i < postsNodes.length; i++) {
-				// 	var post = scrapePost(postsNodes[i]);
-				// 	posts.push(post);
-				// 	if (postMap[post.tx] === undefined) haveNew = true;
-				// 	postMap[post.tx] = post;
-				// }
-
-				// console.log(JSON.stringify(posts));
-				// console.log(posts);
-				// resolve(posts);
 			});
 }
 
