a076074f7a5d7cb97a8495fe3a7a70371dc1fd22
blake <blake@localhost.localdomain>
2023-02-26 21:43:24 -0600

    add donation address

--- fe/public/header.js
+++ fe/public/header.js
@@ -8,6 +8,7 @@ export default function Header() {
 				<a href="/">Home</a>
 				<a href="/todo">To do</a>
 			</nav>
+			<span>bitcoincash:qqqgc2jy07ltnz4ygemcec05jtentqnleu3hjmy2j0</span>
 			<label>
 				URL:
 				<input readonly value={url} ref={c => c && (c.size = c.value.length)} />
